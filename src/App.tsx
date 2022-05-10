c1cc97a0d6c7c277a2975681a576e86a1b6f1270
shogo-115 <67040383+shogo-115@users.noreply.github.com>
2022-05-10 17:33:18 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+import theme from "./theme/theme";
+import { Router } from "./router/Router";
+
+export function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ChakraProvider>
+  );
+}
